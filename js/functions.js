db1f17463c0ca360021bfdcd151db7cbcbbfdd1c
SommerAntje <info@antje-sommer.de>
2019-07-22 15:23:56 +0200

    remove comments

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,3 @@
-// wenn der browser ein IE ist soll das hashchange event getriggert  werden 
-// in einem Intervall von 20ms wird geprüft ob sich der hash ändert
 (function() {
     if (!("onhashchange" in window)) {
         var oldHash = location.hash;
@@ -12,89 +10,53 @@
     }
 })();
 $(function(){
-	// wenn ein bookmarklink aufgerufen wird, soll die entsprechende seite angezeigt werden
 	if(location.hash){
-		// location.hash wird um 'page_' erweitert = id der seite im html
 		var bookmark ='#page_'+ location.hash.substring(1);
-		// index der 'bookmarkseite' ermitteln, um diesen beim contentslide initialisieren zu übergeben
 		var bookmarkIndex  = $(bookmark).index();
-		
 	} else {
-		// usability: beim ersten aufruf der seite, soll die index.html den hash 'start' besitzen
 		location.hash = '#Start';
-		// und die startseite soll angezeigt werden
 		bookmarkIndex = 0; 
 		
 	};
-	// wenn sich der hash ändert bspw. beim zurückblättern in der history
-	// zeige analog die entsprechende seite an
 	$(window).on('hashchange', function(e, hashIndex){
-		// location.hash wird um 'page_' erweitert = id der seite im html
 		var hash = '#page_'+location.hash.substring(1);
-		// index der aktuell angezeigten seite im hash ermitteln
 		var hashIndex = $(hash).index();
-		// das hashchange event,  wird über die navigation getriggert
-		// über die navigation wird das contentslideplugin gesteuert
 		$('#wrapper').trigger('showByIndex.contentSlide',hashIndex);
 	});
 
-	// contentslide plugin initialisieren
 	$('#wrapper').contentSlide({init: bookmarkIndex }); 
-	
-	/*********das contentslideplugin soll von außen über die navigation gesteruert werden********/
+
 	$('#nav a').on('click', function(e){
 		e.preventDefault();
 		
-		var menuIndex = $(this).parent().index(); // ermittelt den index des geklickten linkitems
-		// contentslide wird über hash gesteuert
+		var menuIndex = $(this).parent().index(); 
 		location.hash = $('#wrapper > div').eq(menuIndex).attr('id').substring(5); 
-		// beim Klick auf den Menupunkt der aktuellen Seite 'Hauptnavigation' sollen die 'Slider' zurückgesetzt werden
 		$('#wrapper > div').eq(menuIndex).find('.slides').trigger('showByIndex.contentSlide', 0);
 		$('#wrapper > div').eq(menuIndex).find('.slide_menu a').removeClass('active');
 	});
-		// #wrapper wird durch contentSlide zu den koordinaten der unterseiten 'geschoben'
 	$('#wrapper').on('pageEnter', function(evt, params){
 	 	if (params.container.get(0) != this){return false;}
-		
-		// beim verlassen der aktuellen seite sollen die 'Slider' zurückgesetzt werden
 	 	$('#wrapper > div').eq(params.pageIndLeft).find('.slides').trigger('showByIndex.contentSlide', 0);
-	 	// highlighting der Untermenus wird entfernt, wenn auf eine  andere Seite geklickt wird
 	 	$('#wrapper > div').eq(params.pageIndLeft).find('.slide_menu a').removeClass('active');
-
-	 	// beim verlassen der seite sollen untermenus verborgen sein
 		$('#wrapper > div').eq(params.pageIndLeft).find('.slide_menu li:first-child').siblings().hide();
-	 	
-	 	// Bildergalerien der Untermenus werden bei der jew. aktivierten Seite 
-		// auf einen optionalen Index 'showByIndex' gesetzt. 
-		// Eventhandling: der Index ist in der show function() des image_gallery Plugin definiert
-	 	// beim verlassen der aktuellen seite werden 'Bildergalerien' auf optionalen index zurückgesetzt
 	 	$('#wrapper > div').eq(params.pageIndLeft).find('.gallery').trigger('showByIndex.imageGallery', 0);
-		
-		// beim eintritt jeder aktuellen seite sollen die bilder der bildergalerien nachgeladen werden
-		// finde alle bilder auf der aktuellen seite 
 		$('#wrapper > div').eq(params.pageIndEnter).find('img[data-src]').each(function(){
-			// wenn das bild über das attribut data-src verfügt 'wobei src die referenz zu einem platzhalter gif enthält'
-			// hole referenz vom inhalt des attributs data-src
-			// und tausche diesen gegen die referenz von src 
 			var $this = $(this);
 			$this.attr('src', $this.attr('data-src'));
-			// lade den inhalt zu der ausgetauscheten referenz von src
 			$this.removeAttr('data-src');
-			// entferne das attr data-src mit ausgetauschtem inhalt
 		});
 	});
 
 	/****page_Food****/
-	// contentSlide Plugin wird initialisiert
+
 	$('.slides').contentSlide({init: 0});
 	
-	// beim aufruf der seite sollen untermenus verborgen sein
 	$('#wrapper > div').find('.slide_menu li:first-child').siblings().hide();
 
 	$('.slide_menu a , .slides div.rubrik > a ').on('click', function(evt){
 		evt.preventDefault();
-		// ermittelt den index des geklickten linkitems
 		var index = $(this).parent().index();
+		
 		if($(this).parent().is('div.rubrik')){index += 1}
 
 		$(this).closest('#wrapper > div').find('.slides').trigger('showByIndex.contentSlide', index)
@@ -107,19 +69,15 @@ $(function(){
 	 	if (params.container.get(0) != this){return false;}
 
 	 	if($('#page_Team').index() == params.pageIndEnter){
-			// beim Eintritt der page_Team 
-			// soll die Bildergalerie 'cuisine_gallery' in einem Interval ablaufen
 				
 			var galleryInterval = function(selectGallery){
-				// anzahl der Bilder
 				var galleryItems = $(selectGallery + ' li');
 				var ImgLength = galleryItems.length,
-				// index des aktuell angezeigten bildes
-				// hoch zählen
 				    indImg = galleryItems.filter(':visible').index()+1;
-				// zurücksetzen 'loop' wenn letztes bild erreicht ist
-				if(indImg == ImgLength){indImg = 0;}
-				$(selectGallery).trigger('showByIndex.imageGallery',indImg);
+				
+					if(indImg == ImgLength){indImg = 0;}
+				
+					$(selectGallery).trigger('showByIndex.imageGallery',indImg);
 			};
 				var pageTeamInterval = setInterval(function(){
 					galleryInterval("#cuisine .gallery ul");   
@@ -132,7 +90,7 @@ $(function(){
 		}
 	});
 	
-	/***Navigationimages hover: page_Food // page_Beverages***/
+	/***Navigation images hover: page_Food // page_Beverages***/
 	$('.slides div.rubrik a img').hover(
 		function(){
 		    $(this).css({opacity:'1.0'});
@@ -142,8 +100,8 @@ $(function(){
 		}
 	);
 
-	/****page_Kunden****/
-	// Bedingung für den IE8
+	/****page_clients****/
+	// IE8
 	if($('html').hasClass('ie8')){
 		$('.ie8 #page_Kunden p').css({"visibility":"hidden"}); 
 		$('.ie8 #page_Kunden span').hover(
@@ -155,7 +113,7 @@ $(function(){
 			}
 		);
 	} else {
-	// Bedingung für alle anderen Browser, wenn nicht IE8
+	//!IE8
 		$('#page_Kunden p').animate({opacity:0.01}); 
 		$('#page_Kunden span').hover(
 			function(){
@@ -168,7 +126,6 @@ $(function(){
 	};
 	
 	/****page_Jobs****/
-		// scrollbar
 	$('.scrollable').slimScroll({
 		height: '250px',
 		width: '670px',
@@ -176,7 +133,6 @@ $(function(){
 		alwaysVisible: 'true'
 	});
 
-	/****allgemein****/
 	$('#nav h1 a img').css({left:-500}).animate({left:-75},1000);
 	$('#nav').css({bottom:-50}).animate({bottom:20},1500);
 	$('#nav li').on('click',function(){
@@ -184,22 +140,15 @@ $(function(){
 	 		   .siblings().removeClass('active');
 	});
 
-	/****initialisieren der einzelnen Bildergalerien****/
 	$('.gallery > ul').imageGallery();
 
-	/****Vor- und Zurückbutton der einzelnen Bildergalerien****/
-		// click weiter
 	$('.button_next').on('click', function(){
 	 	$(this).prevAll('ul').trigger('showByRelativeIndex.imageGallery', 1);
-	});
-		// click zurück 
+	}); 
 	$('.button_prev').on('click', function(){
 	 	$(this).prevAll('ul').trigger('showByRelativeIndex.imageGallery', -1);
 	});
-	// e-mail adresse maskieren
-	// klasse an jqueryobjekt übergeben
 	var $mascs = $('.email-masc');
-	// austausch des textes in jeder e-mail mit dem attribut data-content 
 	$mascs.each( function(){
 		$(this).html( $(this).attr('data-content').replace('($)', '@').replace(/_xRz_/g,'') );
 	} );
--- js/image_gallery.js
+++ js/image_gallery.js
@@ -8,32 +8,21 @@ Image Gallery
 	"use strict"
 	$.fn.imageGallery = function(options){
 		var settings = {
-			// index zum auswählen des startbildes
 			init : 0
 		};
 		 var option = $.extend(settings, options);
 		 return this.each(function(){
-		 	// auswahl der ul als imagecontainer 
 		 	var $imageContainer = $(this),
-		 	// listitems mit bild und bildbeschreibung
 		 		$listItems = $imageContainer.find('li'),
-		 	// anzahl der bilder ermitteln, die in der ul enthalten sind
+		
 		 		itemLength = $listItems.length,
 		 	
-		 	// ausgangssituation:
-		 	// inital index des items aktuelles bild
-		 		currentImg = option.init;
+		  		currentImg = option.init;
 
-		 	// alle anderen listitems sind nicht sichtbar Reihenfolge BEACHTEN!
 		 		$listItems.not(currentImg).hide();
-			// erstes bild und bildbeschreibung sind sichtbar
+		
 				$listItems.eq(currentImg).show();
 			
-			// next und preview funktionalität wird zusammengefasst von außen gesteuert
-			// dazu wird ein namespace hier 'imageGallery'hinzugefügt um konflikt mit anderen plugins zu vermeiden
-			// und an showByRelativeIndex gekoppelt
-			// eventhandling: von außen wird dann ein optionaler wert übergeben, um den die bilder weiter, oder zurückgeblättert werden sollen
-			// eventhandling erwartet als ersten parameter immer ein event!
 			$imageContainer.on('showByRelativeIndex.imageGallery', function(evt, wert){
 				evt.stopPropagation();
 				$listItems.eq(currentImg).fadeOut(200);
@@ -43,14 +32,11 @@ Image Gallery
 				} 
 				$listItems.eq(currentImg).fadeIn(500);
 			});
-			// plugin zurücksetzten von außen steuern 
+			
 			 $imageContainer.on('showByIndex.imageGallery', function(evt, Index){
 			 	evt.stopPropagation();
-			// setze das aktuelle bild auf den wert des Index
 				currentImg = Index;
-			// verberge das bild welches nicht dem aktuellem reset index entspricht
 				$listItems.not(this).fadeOut(200);
-			// blende bild mit aktuellem reset index ein
 				$listItems.eq(currentImg).fadeIn(500);
 			});
 		});
